0f3008d94d1d47fe2559c567af837a01d00f7c26
B122250 <Nothing_to_see_here>
2021-10-14 20:08:41 +0100

    Made the script faster and removed messy files. I still need to analyse the fastqc results

--- Assignment1_script.sh
+++ Assignment1_script.sh
@@ -2,29 +2,21 @@
 
 #With many files
 #Directory for fastqc results, index files, alligned_reads and counts
-mkdir fastqc_results
-mkdir index_files
-mkdir alligned_reads
-mkdir counts
-mkdir bamfiles
-mkdir final_counts
-
+mkdir fastqc_results && mkdir index_files && mkdir alligned_reads && mkdir counts && mkdir bamfiles && mkdir final_counts
 
 #to allign the reads, I need to make an index of the reference genome in a directory called index_files, only need to do this once, so outside$
-bowtie2-build /localdisk/home/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz index_files/index_ref_file
+bowtie2-build --threads 60 /localdisk/home/data/BPSM/AY21/Tcongo_genome/TriTrypDB-46_TcongolenseIL3000_2019_Genome.fasta.gz index_files/index_ref_file
 
 #now looping for every file
 for file in /localdisk/home/data/BPSM/AY21/fastq/*.fq.gz
 do
 #-f flag used, to specify the input format, in this case fastq
 # For future versions, I might want to use the -t flag,to specify the threads
-fastqc -t 6 -f fastq $file -o $PWD/fastqc_results
+fastqc -t 60 -f fastq $file -o $PWD/fastqc_results
 done
 
 #making a file that has both forwards and reverse reads on the same line, so that I can use a single variable for both
-touch forward_and_reverse_reads.txt
-touch forward_reads.txt
-touch reverse_reads.txt
+touch forward_and_reverse_reads.txt && touch forward_reads.txt && touch reverse_reads.txt
 for file in /localdisk/home/data/BPSM/AY21/fastq/*1.fq.gz
 do
 echo "${file:37}" >> forward_reads.txt
@@ -43,7 +35,7 @@ touch genes.txt
 echo "Gene	Gene_description">>genes.txt
 cut -f 4,5 /localdisk/home/data/BPSM/AY21/TriTrypDB-46_TcongolenseIL3000_2019.bed >>genes.txt
 #make a copy for the final gene counts
-cp genes.txt final_counts/gene_counts_1.txt
+cp genes.txt final_counts/gene_counts_0.txt
 
 cat forward_and_reverse_reads.txt | while read line
 do
@@ -51,12 +43,12 @@ input_file_1=$(echo ${line:0:21})
 input_file_2=$(echo ${line:21})
 sam_name=${line:0:13}
 #the allignment itself, with output in alligned_reads
-bowtie2 -x index_files/index_ref_file -1 /localdisk/home/data/BPSM/AY21/fastq/$input_file_1 -2 /localdisk/home/data/BPSM/AY21/fastq/$input_file_2 -S alligned_reads/$sam_name.aligned.sam
+bowtie2 -p 60 -x index_files/index_ref_file -1 /localdisk/home/data/BPSM/AY21/fastq/$input_file_1 -2 /localdisk/home/data/BPSM/AY21/fastq/$input_file_2 -S alligned_reads/$sam_name.aligned.sam
 echo "$sam_name" >> sam_filenames.txt
 done
 
-i=$(1)
-p=$(2)
+i=0
+p=1
 
 #convert output to indexed bam
 cat sam_filenames.txt | while read samfile
@@ -94,4 +86,9 @@ paste -d'\t' final_counts/gene_counts_$i.txt temp_gene_counts_single_columns.txt
 ((i=i+1))
 ((p=p+1))
 done
-
+#Select the last file, which is the one containing all the gene counts
+last_file=$(wc -l  sam_filenames.txt | cut -d ' ' -f1)
+mv final_counts/*$last_file.txt replicate_gene_expressions.txt
+#Remove messy files and directories
+rm -fr forward_and_reverse_reads.txt && rm -fr forward_reads.txt && rm -fr reverse_reads.txt && rm -fr temp_gene_counts_single_columns.txt && rm -fr temp_gene_counts.txt && rm -fr temp_count_file.txt && rm -fr genes.txt && rm -fr sam_filenames.txt
+rm -fr counts && rm -fr final_counts
